6118cf124eff66418ca0d45c0a095cbb1cf101ff
7 Partidas Digital <7partidasdigital@gmail.com>
2018-01-26 20:01:06 +0100

    Primera versión

---
+++ galdosenr/R/episodios.r
@@ -0,0 +1,126 @@
+#' Episodios nacionales de Benito Pérez Galdós
+#'
+#' Este paquete contiene los textos completos de los Episodios Nacionales
+#' de la serie 1.ª publicados entre 1873 y 1875 y formateados para
+#' un correcto análisis textual. Los seis primeros elementos de cada
+#' vector son el título, el autor y el número del episodios separados
+#' por un elemento en vacío.
+#' @name galdosenr
+#' @docType package
+NULL
+
+#' El texto del EN "Trafalgar"
+#'
+#' Un dataset que contiene el texto "Trafalgar", el primer Episodio Nacional
+#' de Beneito Pérez Galdós, publicado en 1873. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Trafalgar&oldid=196685}
+#' @format Vector de caracteres con 4853 elementos.
+"trafalgar"
+
+#' El texto del EN "La corte de Carlos IV"
+#'
+#' Un dataset que contiene el texto "La corte de Carlos IV", esegundo Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1873. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=La_corte_de_Carlos_IV&oldid=502348}
+#' @format Vector de caracteres con 7326 elementos.
+"corte_carlos_iv"
+
+#' El texto del EN "El 19 de marzo y el 2 de mayo"
+#'
+#' Un dataset que contiene el texto "El 19 de marzo y el 2 de mayo", tercer Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1873. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=El_19_de_marzo_y_el_2_de_mayo&oldid=502350}
+#' @format Vector de caracteres con 6737 elementos.
+"marzo19_2mayo"
+
+#' El texto del EN "Bailén"
+#'
+#' Un dataset que contiene el texto "Bailén", cuarto Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1873. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Bail\%C3\%A9n&oldid=196724}
+#' @format Vector de caracteres con 6101 elementos.
+"bailen"
+
+#' El texto del EN "Napoleón en Chamartín"
+#'
+#' Un dataset que contiene el texto "Napoleón en Chamartín", quinto Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1874. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Napole\%C3\%B3n_en_Chamart\%C3\%ADn&oldid=196683}
+#' @format Vector de caracteres con 7868 elementos.
+"napoleon_en_chamartin"
+
+
+#' El texto del EN "Zaragoza"
+#'
+#' Un dataset que contiene el texto "Zaragoza", sexto Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1874. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Zaragoza&oldid=196687}
+#' @format Vector de caracteres con 6472 elementos.
+"zaragoza"
+
+
+#' El texto del EN "Gerona"
+#'
+#' Un dataset que contiene el texto "Gerona", séptimo Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1874. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Gerona&oldid=196675}
+#' @format Vector de caracteres con 5850 elementos.
+"gerona"
+
+
+#' El texto del EN "Cádiz"
+#'
+#' Un dataset que contiene el texto "Cádiz", octavo Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1874. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=C\%C3\%A1diz&oldid=196671}
+#' @format Vector de caracteres con 8846 elementos.
+"cadiz"
+
+
+#' El texto del EN "Juan Martín el Empecinado"
+#'
+#' Un dataset que contiene el texto "Juan Martín el Empecinado", noveno Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1874. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=Juan_Mart\%C3\%ADn_el_Empecinado&oldid=196677}
+#' @format Vector de caracteres con 7040 elementos.
+"empecinado"
+
+
+#' El texto del EN "La batalla de los Arapiles"
+#'
+#' Un dataset que contiene el texto "Juan Martín el Empecinado", décimo Episodio Nacional
+#' de Benito Pérez Galdós, publicado en 1875. El texto en codificación UTF-8 se
+#' obtuvo de Wikisource, se revisó y corrigió. Está dividido en elementos
+#' de 80 caracteres. Los párrafos están separados por un elemento en blanco.
+#'
+#' @source \url{https://es.wikisource.org/w/index.php?title=La_batalla_de_los_Arapiles&oldid=228663}
+#' @format Vector de caracteres con 10254 elementos.
+"batalla_arapiles"
+
---
+++ galdosenr/README.md
@@ -0,0 +1,36 @@
+
+# galdosenr
+
+## Paquete de R con los Episodios Nacionales (1ª. serie)
+
+
+Este paquete permite acceder al texto completo de los diez primeros *Episodios Nacionales* de Benito Pérez Galdós. Los textos planos de cada uno de los episodios se obtuvo de [Wikisource](https://es.wikisource.org/wiki/Episodios_nacionales), y han sido revisados y corregidos. Cada uno de los textos es un vector de caracteres en los que cada uno de ellos tiene un máximo de 80 caracteres. El paquete contiene:
+
+* `trafalgar`: *Trafalgar*, publicado en 1873
+* `corte_carlos_iv`: *La corte de Carlos IV*, publicado en 1873
+* `marzo19_2mayo`: *El 19 de marzo y el 2 de mayo*, publicado en 1873
+* `bailen`: *Bailén*, publicado en 1873
+* `napoleon_en_chamartin`: *Napoleón en Chamartín*, publicado en 1874
+* `zaragoza`: *Zaragoza*, publicado en 1874
+* `gerona`: *Gerona*, publicado en 1874
+* `cadiz`: *Cádiz*, publicado en 1874
+* `empecinado`: *Juan Martín el Empecinado*, publicado en 1874
+* `batalla_arapiles`: *La batalla de los Arapiles*, publicado en 1875
+
+
+Quien utilice este paquete debe tener en cuenta que, debido al origen desconocido último, pero sospechado, de los textos, hay pequeñas diferencias de un texto a otro. Se ha de tener en cuenta que siguen las reglas de acentuación y prefijación *antiguas*.
+
+## Instalación
+
+Para instalar el paquete escriba lo siguiente:
+
+```
+library(devtools)
+install_github("7partidasdigital/galdosenr")
+library(galdosenr)
+```
+
+## Cómo emplear este paquete
+
+
+Los textos están disponibles bajo la [Licencia Creative Commons Atribución-CompartirIgual 3.0](https://creativecommons.org/licenses/by-sa/3.0/deed.es); pueden aplicarse términos adicionales.
