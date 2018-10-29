5632f16ebe9fda5204f9501db11889d09e9f9800
7 Partidas Digital <7PartidasDigital@gmail.com>
2018-10-29 21:00:24 +0100

    Mejora de nodes

--- scripts/cosechar_mensajes.R
+++ scripts/cosechar_mensajes.R
@@ -4,8 +4,12 @@ for (i in 1:nrow(listado)){
   url <- listado$Direccion[i]
   discurso <- read_html(url)
   mensaje <- discurso %>%
-    html_node(".wysiwyg") %>%
+    html_nodes(".wysiwyg") %>%
+    html_nodes(":not(.resaltar)") %>%
     html_text()
   salida <- paste(listado$Año[i], "txt", sep=".")
   writeLines(mensaje, salida)
 }
+
+
+url <- "http://www.casareal.es/ES/Actividades/Paginas/actividades_discursos_detalle.aspx?data=5632"
