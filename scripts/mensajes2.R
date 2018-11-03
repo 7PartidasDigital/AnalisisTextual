69196776af8a0e9a767b0803a48d3978ab45a1d1
7 Partidas Digital <7PartidasDigital@gmail.com>
2018-11-03 13:33:53 +0100

    Avanzando

--- scripts/mensajes2.R
+++ scripts/mensajes2.R
@@ -1,25 +1,44 @@
 library(tidyverse)
 library(tidytext)
 
-annos <- c("1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
-duracion <- c("5:01", "7:31", "9:23", "8:13", "19:26", "9:05", "7:26", "8:34", "11:05", "8:44", "9:20", "10:29", "7:20", "8:21", "9:59", "10:33", "10:36", "12:44", "11:45", "12:46", "14:09", "12:25", "11:20", "12:17", "11:47", "7:04", "8:26", "11:02", "13:03", "12:03", "12:22", "12:40", "14:14", "13:46", "12:41", "9:33", "13:04", "8:56", "11:37", "12:40", "12:11", "12:20", "10:47")
+ficheros <- list.files(pattern = "*.txt")
+MetaDatos <- read_tsv("../MetaDatos.txt", col_names = T, col_types = cols(anno = col_character()))
 
-ficheros <- list.files()
-# Esto los lee y los guarda en MesnajesNavidad
+# Esto los lee y los guarda en MensajesNavidad
 MensajesNavidad <- data_frame(ficheros) %>%
   mutate(texto = map(ficheros, read_lines)) %>%
   rename(anno = ficheros) %>%
   unnest()
-
+# Borra de la columna anno la extensión de fichero .txt
 MensajesNavidad$anno <- gsub("\\.txt", "", MensajesNavidad$anno, perl =T)
-# Convierte el $ficheros en factor
-MensajesNavidad$anno <- as_factor(MensajesNavidad$anno)
 
+# Une a MensajesNavidad los MetaDatos: rey y duración en segundos de cada mensaje
+MensajesNavidad <- left_join(MensajesNavidad, MetaDatos, by ="anno")
+
+# Convierte rey y anno a factores (Ya veremos para qué) y numera cada párrafo de cada
+# mensaje.
 MensajesNavidad <- MensajesNavidad %>%
+  mutate_at(c("anno", "rey"), funs(factor(.))) %>%
   group_by(anno) %>%
   mutate(parrafo = row_number()) %>%
-  ungroup() #%>%
+  ungroup()
+# Reordena las columnas
+MensajesNavidad <- MensajesNavidad[, c(1,3,4,5,2)]
+# No se suele recomendar, pero para tener un poco más limpio el Environment,
+# borro dos objetos que ya no harán falta:
+rm(MetaDatos,ficheros)
 
-MensajesNavidad_tidy
+# Ya tenemos el texto de todos los Mensaje recogidos en una gran tabla de cinco
+# columnas Anno, rey, duracion, parrafo, texto. Mantener los párrafos y numerarlos
+# es interesante porque podremos extraer informaciones interesantes.
+
+# Ahora lo vamos a dividir en oraciones.
+MensajesNavidad_oraciones <- MensajesNavidad %>%
+  unnest_tokens(oraciones, texto, token = "sentences") %>%
+  group_by(anno,parrafo) %>%
+  mutate(oracion_id = row_number()) %>%
+  ungroup()
+MensajesNavidad_oraciones <- MensajesNavidad_oraciones[, c(1,2,3,4,6,5)]
+
 
 
