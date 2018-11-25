5ad2ec8b4b73077bd1fa9830b6c6581485dc4abd
7 Partidas Digital <7PartidasDigital@gmail.com>
2018-11-25 19:56:17 +0100

    Ampliado

--- mensajes/enmarcha.R
+++ mensajes/enmarcha.R
@@ -2,36 +2,39 @@ discurso <- readLines("corpus/1975.txt")
 discurso
 library(tidyverse)
 library(tidytext)
-mensaje_tidy <- data_frame(parrafo = seq_along(discurso), texto = discurso)
-mensaje_tidy
-mensaje_palabras <- mensaje_tidy %>%
+mensajes <- data_frame(parrafo = seq_along(discurso), texto = discurso)
+mensajes
+mensajes_palabras <- mensajes %>%
   unnest_tokens(palabra, texto)
-
-mensaje_tidy %>%
+mensajes %>%
   unnest_tokens(palabra, texto, to_lower = F)
-mensaje_oraciones <- mensaje_tidy %>%
+mensaje_oraciones <- mensajes %>%
   unnest_tokens(oraciones, texto, token = "sentences", to_lower = F)
-
-mensaje_palabras %>%
+mensajes_palabras %>%
   count(palabra, sort = T)
-
-mensaje_oraciones <- mensaje_tidy %>%
+mensajes_oraciones <- mensajes %>%
   unnest_tokens(oraciones, texto, token = "sentences") %>%
   mutate(NumPal = str_count(oraciones, "\\w+"))
 
+# Segundo post
 
-
-ficheros <- list.files(path ="corpus", pattern = "*.txt")
+ficheros <- list.files(path ="corpus", pattern = "\\d+")
 anno <- as.numeric(gsub("\\.txt", "", ficheros, perl = T))
-mensaje_tidy <- data.frame(anno = integer(), parrafo = integer(), texto = character(), stringsAsFactors = F)
-mensaje_tidy <- as_tibble(mensaje_tidy)
+mensajes <- data_frame(anno = integer(), parrafo = integer(), texto = character())
 for (i in 1:length(ficheros)){
-  for (i in 1:2){
   discurso <- readLines(paste("corpus", ficheros[i], sep = "/"))
-  mensaje <- data_frame(anno = anno[i], parrafo = seq_along(discurso), texto = discurso)
-  mensaje_tidy <- bind_rows(mensaje_tidy, mensaje)
+  temporal <- data_frame(anno = anno[i], parrafo = seq_along(discurso), texto = discurso)
+  mensajes <- bind_rows(mensajes, temporal)
 }
-  
 
-  
-}
+mensajes_palabras <- mensajes %>%
+  unnest_tokens(palabra, texto)
+
+mensajes_palabras %>%
+  count(palabra, sort = T)
+
+vacias <- read_csv("https://raw.githubusercontent.com/7PartidasDigital/AnalisisTextual/master/mensajes/vacias.txt", col_names = TRUE)
+
+mensajes_palabras %>%
+  anti_join(vacias) %>%
+  count(palabra, sort = T)
